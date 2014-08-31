b31a1eacc7b825db4c5728af45da66d45d663b71
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2014-08-31 16:04:03 -0400

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./deploy.sh <dotfiles directory>"
+    exit
+fi
+
+DIR=$(readlink -f $1)
+
+# bashrc
+rm ~/.bashrc
+ln -s ${DIR}/.bashrc ~/.bashrc
+
+# vimrc
+rm ~/.vimrc
+ln -s ${DIR}/.vimrc ~/.vimrc
+
+# vim plugins
+rm -rf ~/.vim
+ln -s ${DIR}/vim ~/.vim
+
+# gitconfig
+rm ~/.gitconfig
+ln -s ${DIR}/.gitconfig ~/.gitconfig
+
+# awesome
+rm ~/.config/awesome/rc.lua
+rm ~/.config/awesome/run_once.sh
+ln -s ${DIR}/awesome/rc.lua ~/.config/awesome/rc.lua
+ln -s ${DIR}/awesome/run_once.sh ~/.config/awesome/run_once.sh
+
+# feh
+rm ~/.fehbg
+ln -s ${DIR}/.fehbg ~/.fehbg
+
---
+++ vim/ftplugin/python.vim
@@ -0,0 +1,10 @@
+set tabstop=4
+set expandtab
+set softtabstop=4
+set shiftwidth=4
+filetype indent on
+au BufEnter,BufRead *.py setlocal smartindent cinwords=if,elif,else,for,while,try,except,finally,def,class
+"set listchars=tab:>-,trail:$
+"set list
+set showmatch
+set colorcolumn=80
